54616c30e7534879acc218003d662301ba329c44
Ganesh Utla <ganeshutla888@gmail.com>
2023-10-10 18:38:15 +0530

    fixed home while no user

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,7 +13,7 @@ export default async function Home({
 }) {
 
   const user = await currentUser();
-  if (!user) return null;
+  if (!user) return redirect("/sign-in");
 
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
